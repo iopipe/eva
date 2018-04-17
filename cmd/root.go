0a6718776000eaa64f917ec03fe85299dba4d8da
Erica Windisch <73207+ewindisch@users.noreply.github.com>
2018-04-17 15:55:14 +0000

    remove unnecessary inits
    Signed-off-by: Erica Windisch <73207+ewindisch@users.noreply.github.com>

--- cmd/root.go
+++ cmd/root.go
@@ -60,10 +60,6 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
-	//initMakeEvent()
-	//initHTTPListener()
-	//rootCmd.AddCommand(cmdGetUrl)
 }
 
 // initConfig reads in config file and ENV variables if set.
