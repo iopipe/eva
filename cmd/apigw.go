dbc948c3d06ebc0f4267eb9affaf99be83e9dcfc
Erica Windisch <73207+ewindisch@users.noreply.github.com>
2018-04-20 16:16:17 +0000

    Use SetHttpCobraFlags for cloudfrontCmd
    Signed-off-by: Erica Windisch <73207+ewindisch@users.noreply.github.com>

--- cmd/apigw.go
+++ cmd/apigw.go
@@ -4,41 +4,19 @@ import (
 	"fmt"
 	"github.com/iopipe/eva/templates"
 	"github.com/spf13/cobra"
-	"net/http"
-	"net/url"
 )
-
-var cmdFlagHttpHost string
-var cmdFlagHttpUri string
-var cmdFlagHttpAuthorization string
-
-func CliParseHTTP(cmd *cobra.Command, args []string) *http.Request {
-  request := &http.Request{
-    Method: "GET",
-    URL: &url.URL{
-      Path: cmdFlagHttpUri,
-    },
-  }
-  return request
-}
 
 var apigwCmd = &cobra.Command{
 	Use:   "apigw",
 	Short: "Generate an API Gw event.",
-	Run: func (cmd *cobra.Command, args []string) {
-   request := CliParseHTTP(cmd, args)
-   result := templates.CreateApiGwEvent(request)
-   fmt.Println(result)
-  },
-}
-
-func SetHttpCobraFlags (command *cobra.Command) {
-	command.Flags().StringVarP(&cmdFlagHttpHost, "host", "H", "", "HTTP(s) host for event data.")
-	command.Flags().StringVarP(&cmdFlagHttpUri, "path", "p", "", "HTTP(s) path or uri.")
-	command.Flags().StringVarP(&cmdFlagHttpAuthorization, "auth", "A", "", "Authorization header")
+	Run: func(cmd *cobra.Command, args []string) {
+		request := CliParseHTTP(cmd, args)
+		result := templates.CreateApiGwEvent(request)
+		fmt.Println(result)
+	},
 }
 
 func init() {
-  SetHttpCobraFlags(apigwCmd)
+	SetHttpCobraFlags(apigwCmd)
 	generateCmd.AddCommand(apigwCmd)
 }
--- cmd/cloudfront.go
+++ cmd/cloudfront.go
@@ -4,33 +4,19 @@ import (
 	"fmt"
 	"github.com/iopipe/eva/templates"
 	"github.com/spf13/cobra"
-	"net/http"
-	"net/url"
 )
-
-var cmdFlagMakeEventHost string
-var cmdFlagMakeEventUri string
-var cmdFlagMakeEventAuthorization string
 
 var cloudfrontCmd = &cobra.Command{
 	Use:   "cloudfront",
 	Short: "Generate a cloudfront event.",
 	Run: func(cmd *cobra.Command, args []string) {
-		request := &http.Request{
-			Method: "GET",
-			URL: &url.URL{
-				Path: cmdFlagMakeEventUri,
-			},
-		}
+		request := CliParseHTTP(cmd, args)
 		result := templates.CreateCloudfrontEvent(request)
 		fmt.Println(result)
 	},
 }
 
 func init() {
-	cloudfrontCmd.Flags().StringVarP(&cmdFlagMakeEventHost, "host", "H", "", "HTTP(s) host for event data.")
-	cloudfrontCmd.Flags().StringVarP(&cmdFlagMakeEventUri, "path", "p", "", "HTTP(s) path or uri.")
-	cloudfrontCmd.Flags().StringVarP(&cmdFlagMakeEventAuthorization, "auth", "A", "", "Authorization header")
-
+	SetHttpCobraFlags(cloudfrontCmd)
 	generateCmd.AddCommand(cloudfrontCmd)
 }
